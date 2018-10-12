805499c5cf734b7ebd89e51e8c1358ce4068899b
saidi zied <ziedsaidig@gmail.com>
2018-10-12 02:27:09 +0100

    add Readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+
+
+
+######Test  
+
+
+all the code exist inside the solutions folder...
+
+
+##### to start the script type 
+   $  npm start
--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const    {f1,f2,f3,f4} = require('./solutions/solutions');
 
 
 
+
+//Testing ...
  console.log(f1(['aba', 'xzxb', 'ab'], ['aba', 'baba', 'aba', 'xzxb']))
